3fee54a0e81e6507c6e549d58e9464e131ef7e9b
⬆️ bald <kenteguchi70@gmail.com>
2026-08-06 09:53:25 -0400

    added more cats

--- player.py
+++ player.py
@@ -75,18 +75,28 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("battle cats (plz dont sue me)")
 clock = pygame.time.Clock()
-image = pygame.image.load("Tophat.png").convert_alpha() 
-image = pygame.transform.scale(image, (50, 50))
+tophat = pygame.image.load("Tophat.png").convert_alpha() 
+tophat = pygame.transform.scale(image, (50, 50))
+rizz = pygame.image.load("RizzKit.png").convert_alpha() 
+rizz = pygame.transform.scale(image, (50, 50))
+angy = pygame.image.load("AngyCat.png").convert_alpha() 
+angy = pygame.transform.scale(image, (50, 50))
 mana = 0.0
 font = pygame.font.Font(None, 50)
 
-cats = [(680, 480)]  
+tophat_cats = [(680, 480)] 
+rizz_kits = [(580, 480)]
+angy_cars = [480, 480)]
 running = True
 while running:
     x = font.render(str(int(mana)), True, (0, 0, 0))
     screen.fill((255, 255, 255))
-    for cat in cats:
-        screen.blit(image, cat)
+    for cat in tophat_cats:
+        screen.blit(tophat, cat)
+    for cat in rizz_kits:
+        screen.blit(rizz, cat)
+    for cat in angy:
+        screen.blit(angy, cat)
     screen.blit(x, (100, 100))
     pygame.display.flip()
     for event in pygame.event.get():
